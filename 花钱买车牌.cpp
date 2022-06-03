556df0473aca0b2f117ae447134015553870de6f
1diamondplus <106752994+1diamondplus@users.noreply.github.com>
2022-06-03 20:17:56 +0800

    Add files via upload

---
+++ 花钱买车牌.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+struct kkk{
+	int a_i;
+	int cnt;
+}a[10005];
+int main()
+{
+	int n,k,s[10005],kk[10005],u,i;
+	char b[10005];
+	cin>>n>>k;
+	cin>>b;
+	for(i=0;i<n;i++)
+	{
+		s[i]=b[i]-'0';
+		kk[i]=s[i];
+//		cout<<kk[i]<<' '; 
+	}
+	sort(s,s+n);
+	a[0].a_i=s[0],a[0].cnt=0;
+	u=0;
+	for(i=0;i<n;i++) 
+	{
+		if(s[i]!=a[u].a_i)
+		{
+			u++;
+			a[u].a_i=s[i];
+			a[u].cnt=1;
+		}
+		else
+		a[u].cnt++;
+	}
+//	cout<<u<<endl;
+	for(i=0;i<=u;i++)
+	{
+//		cout<<a[i].cnt<<' '<<a[i].a_i<<endl;
+		
+		if(a[i].cnt==k)
+		{
+			for(i=0;i<n;i++)
+			{
+				cout<<kk[i];
+			}
+			return 0;
+		}
+		
+	}
+	return 0;
+}
+
